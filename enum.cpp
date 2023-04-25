c513b519516ee13ece548e3f4e727c271187ad0c
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-04-25 21:42:47 +0530

    Add files via upload

---
+++ enum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+enum week{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday};
+
+int main(){
+    week today;
+    today=Tuesday;
+    cout<<"Day "<<today+1;
+    return 0;
+}
