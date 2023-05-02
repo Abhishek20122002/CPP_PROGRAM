eb6f23cbee7abb8cbddf11254f6f13dc1934c74e
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-05-02 20:51:13 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<class T>
+void display(vector<T> &v){
+    cout<<"Displaying this vector"<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    vector<int> vec1;
+    display(vec1);
+    vector<char> vec2(4);
+    vec2.push_back('5');
+    display(vec2);
+    vector<char> vec3(vec2);
+    display(vec3);
+    vector<int> vec4(6,13);
+    display(vec4);
+    return 0;
+}
